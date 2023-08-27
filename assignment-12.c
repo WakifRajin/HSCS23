e725696e8752377e7266813f85960c34853dc3a3
Azwad Wakif <69780475+WakifRajin@users.noreply.github.com>
2023-08-27 10:15:41 +0600

    Update assignment-12.c

--- assignment-12.c
+++ assignment-12.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
-int main()
-{
-  int year;
-  scanf("%d", &year);
-  if(year%4==0 && year%100!=0)
-  {
-      printf("%d is a leap year", year);
-  }
-  else
-  {
-      printf("%d is not a leap year", year);
-  }
-  return 0;
+
+int main() {
+    int year;
+
+    printf("Enter a year: ");
+    scanf("%d", &year);
+
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        printf("%d is a leap year.\n", year);
+    } else {
+        printf("%d is not a leap year.\n", year);
+    }
+
+    return 0;
 }
