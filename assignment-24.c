d8230f884bff05b88cc660c565797e35c3e89229
Azwad Wakif <69780475+WakifRajin@users.noreply.github.com>
2023-08-28 18:41:28 +0600

    Update assignment-24.c

--- assignment-24.c
+++ assignment-24.c
@@ -5,7 +5,7 @@ int main()
 {
   char name[80];
   int name_length;
-  scanf ("%s", &name);
+  scanf ("%s", name);
   name_length = strlen (name);
   printf ("The length of my name %s is %d characters.\n", name, name_length);
   return 0;
